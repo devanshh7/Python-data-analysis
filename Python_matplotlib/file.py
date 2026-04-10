908c33d41fe2b6254a020c833b5073ae86d8b290
devanshh7 <sharmadevansh277@gmail.com>
2026-04-10 15:19:02 +0530

    updated files

--- Python_matplotlib/file.py
+++ Python_matplotlib/file.py
@@ -5,7 +5,7 @@ end = int(input("Enter ending number: "))
 for i in range(start, end + 1):
     filename = f"task{i}.ipynb"  # file name with .ipynb extension
     with open(filename, "w") as f:
-        f.write(f"This is {filename}\n")  # optional content
-    print(f"Created file: {filename}")
+    #   f.write(f"This is {filename}\n")  # optional content
+        print(f"Created file: {filename}")
 
 print("All files created successfully!")
